47c041790eeb24063df9ece1def8d5edd28ca3e4
Dwight Doane <dwight.doane@startupmedia.com>
2016-02-09 14:35:10 -0500

    winner='trainspotting

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -5,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', 'Fight Club'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} winner="Trainspotting" />,
   document.getElementById('app')
 );
