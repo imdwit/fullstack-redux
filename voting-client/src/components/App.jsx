eb462cc60aa568ddcd76e0e9402b45e69f98543b
Dwight <dwightdoane@me.com>
2016-02-10 22:33:16 -0500

    adds tally to component

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', 'Fight Club');
+const tally = Map({'Trainspotting': 4, 'Fight Club': 3});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {pair: pair, tally: tally});
   }
 });
